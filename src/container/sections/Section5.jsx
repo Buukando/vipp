7128221dbadfb119759320fae2f479f7019f7a52
tchiinhemba <tchiinhemba@outlook.com>
2021-12-08 00:13:11 +0100

    update

--- src/container/sections/Section5.jsx
+++ src/container/sections/Section5.jsx
@@ -1,15 +1,37 @@
+// Libs
 import styled from "styled-components";
 
-const Container = styled.section`
-  width: 100%;
-  height: 100vh;
-`;
+// Components
+import MainButton from '../../components/buttons/MainButton';
+
+// Assets
+import Background from '../../assets/img/section-four/four.jpg'
+
 
 export default function Section5() {
+
+  const Container = styled.section`
+    width: 100%;
+    height: 100vh;
+    background: url(${Background}) no-repeat center center;
+    background-size: cover;
+    object-fit: cover;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+  const btnConfigs = {
+    padding: '.8rem 1.2rem',
+    color: 'white',
+    bgColor: 'red',
+    text: 'teste',
+    fontSize: '.5rem'
+  }
   return (
     <Container>
         <div>
-            <button>Look inside Vipp Shelter</button>
+            <MainButton configs={btnConfigs}>Look inside Vipp Shelter</MainButton>
         </div>
     </Container>
   );
