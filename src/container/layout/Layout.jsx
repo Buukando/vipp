27c3e3499910076b2be9db70bf52ee73967a8a76
tchiinhemba <tchiinhemba@outlook.com>
2021-12-12 23:57:30 +0100

    last instagrams posts

--- src/container/layout/Layout.jsx
+++ src/container/layout/Layout.jsx
@@ -5,6 +5,7 @@ import Section4 from '../sections/Section4';
 import Section5 from '../sections/Section5';
 import Section6 from '../sections/Section6';
 import Section7 from '../sections/Section7';
+import Section8 from '../sections/Section8';
 
 
 export default function Layout(props) {
@@ -16,7 +17,8 @@ export default function Layout(props) {
             <Section4></Section4>
             <Section5></Section5>
             <Section6></Section6>
-            <Section7></Section7>       
+            <Section7></Section7>  
+            <Section8></Section8>     
         </div>
     )
 }
